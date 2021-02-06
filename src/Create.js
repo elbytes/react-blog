776735e1eb91fc8be57ebf6b665d87edcde6e655
elbytes <elnaz.alimardani@gmail.com>
2021-02-06 11:19:35 -0500

    implemented routing with react-router-dom

---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+    return (
+        <div className="create">
+            <h2>Create New Blog Post</h2>
+        </div>
+      );
+}
+ 
+export default Create;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return ( <div className="navbar">
         <h1>A React Blog</h1>
         <div className="links">
-            <a href="/">Home</a>
-            <a href="/create">New Blog</a>
+            <Link to="/">Home</Link>
+            <Link to="/create">New Blog</Link>
         </div>
     </div> );
 }
