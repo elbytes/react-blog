c735cb41b62f411a6c58d07ccb140a16fe5dcff1
elbytes <elnaz.alimardani@gmail.com>
2021-02-06 16:26:17 -0500

    handleSubmit

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,42 @@
+import { useState } from 'react';
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mario');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const blog = {title, body, author};
+        console.log(blog);
+    }
     return (
         <div className="create">
             <h2>Create New Blog Post</h2>
+            <form onSubmit={ handleSubmit}>
+            <label>Post Title</label>
+            <input
+                type="text"
+                required 
+                value={ title } 
+                onChange = { e=> setTitle(e.target.value)}
+            />
+            <select
+            required
+            value ={author}
+            onChange = {e => setAuthor(e.target.value)}>
+                <option>Mario</option>
+                <option>Yoshi</option>
+            </select>
+            <label>Post Body</label>
+            <textarea
+                required
+                value={ body }
+                onChange = {e => setBody(e.target.value)}>
+            </textarea>
+            <button>Submit Post</button>
+            </form>
         </div>
       );
 }
